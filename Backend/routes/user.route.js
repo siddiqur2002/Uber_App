61b50047317c2dc9c9f6c389adeeb8dfa450809a
Muhammad Siddiqur Rahman <sidab2002@gmail.com>
2025-03-21 23:33:42 +0600

    login setup is done

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -12,4 +12,11 @@ router.post('/register', [
     userController.registerUser
 )
 
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').isLength({ min: 5 }).withMessage('Password must be 5 character long')
+],
+    userController.loginUser
+)
+
 module.exports = router;
