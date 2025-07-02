a2c67aed5dad582b1ec150e03f47c8c1435ed4a7
Muhammad Siddiqur Rahman <sidab2002@gmail.com>
2025-07-02 23:30:51 +0600

    captain profile, login and logout api test succesfull

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,6 +1,9 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const blackListTokenModel = require('../models/blacklistToken.model');
+const captainModel = require('../models/captain.model');
+
 
 module.exports.authUser = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
@@ -9,7 +12,7 @@ module.exports.authUser = async (req, res, next) => {
         return res.status(401).json({ message: 'unautorized' });
     }
 
-    const isBlacklisted = await userModel.findOne({ token: token });
+    const isBlacklisted = await blackListTokenModel.findOne({ token: token });
 
     if(isBlacklisted){
         return res.status(401).json({ message: 'unauthorized' });
@@ -22,6 +25,34 @@ module.exports.authUser = async (req, res, next) => {
         const user = await userModel.findById(decoded._id);
 
         req.user = user;
+        return next();
+
+    } catch (error) {
+        return res.status(401).json({ message: 'unautorized' });
+    }
+}
+
+
+module.exports.authCaptain = async (req, res, next) => {
+
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
+
+    if(!token){
+        return res.status(401).json({ message: 'unautorized' });
+    }
+
+    const isBlacklisted = await blackListTokenModel.findOne({ token: token });
+
+    if(isBlacklisted){
+        return res.status(401).json({ message: 'unauthorized' });
+    }
+
+    try {
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decoded._id);
+
+        req.captain = captain;
         return next();
 
     } catch (error) {
--- Backend/routes/captain.route.js
+++ Backend/routes/captain.route.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
 const captainController = require('../controllers/captain.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
+
+
 
 router.post('/register', [
 
@@ -17,5 +20,19 @@ router.post('/register', [
 ],
    captainController.registerCaptain 
 );
+
+
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').isLength({ min: 5 }).withMessage('Password must be 5 character long'),
+], 
+    captainController.loginCaptain
+);
+
+
+router.get('/profile', authMiddleware.authCaptain, captainController.getCaptainProfile);
+
+router.get('/logout', authMiddleware.authCaptain, captainController.logoutCaptain);
+
 
 module.exports = router;
